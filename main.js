6511967b7a9842f368899910dfcfa2aea4cac0ff
owlish-character <73556137+owlish-character@users.noreply.github.com>
2020-11-02 20:38:09 +0200

    Add files via upload
    Portfolio website

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
-// 1388833980:AAHH8s0ZumuaQrQiY1YmNQhtrudypQjfNzM
-
-// https://api.telegram.org/bot
-// -430097627
-
-let show = document.getElementById('show');
 let formSection = document.getElementById('formSection');
 let send = document.getElementById('send');
 
-show.onclick = function() {
-    formSection.style.display = 'block';
-    show.style.display = 'none';
-}
 
 send.onclick = function() {
     let name = document.getElementById('name').value;
-    let phoneNumber = document.getElementById('phoneNumber').value;
-    let message = document.getElementById('message').value;
+    let question = document.getElementById('question').value;
     
-    let postInfo = new XMLHttpRequest();
-    postInfo.open('get', `https://api.telegram.org/bot1388833980:AAHH8s0ZumuaQrQiY1YmNQhtrudypQjfNzM/sendMessage?text= %0AName: ${name}%0APhone Number: ${phoneNumber}%0AMessage: ${message}&chat_id=-430097627`, false)
+let postInfo = new XMLHttpRequest();
+    postInfo.open('get', `https://api.telegram.org/bot1388833980:AAHH8s0ZumuaQrQiY1YmNQhtrudypQjfNzM/sendMessage?text= %0AName: ${name}%0AQuestion: ${question}&chat_id=-430097627`, false)
     postInfo.send();
 }
